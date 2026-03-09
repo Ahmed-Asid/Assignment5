b34c64a1843cb23e02bf103e441c437eae9131dd
Ahmed-Asid <ahmedasid249@gmail.com>
2026-03-09 23:47:10 +0600

    added the priority design

--- main.js
+++ main.js
@@ -36,8 +36,19 @@ function displayIssues (issues) {
             labelClass = "btn-primary";
           }
 
-          labelHtml = labelHtml + `<div class="btn btn-soft ${labelClass}">${label}</div>`
+          labelHtml = labelHtml + `<div class="btn btn-soft uppercase ${labelClass}">${label}</div>`
         })
+
+        
+        if (issue.priority === "high"){
+          priorityClass = "btn-error"
+        }
+        else if (issue.priority === "medium"){
+          priorityClass = "btn-warning"
+        }
+        else if (issue.priority === "low"){
+          priorityClass = "btn-accent"
+        }
 
             if (issue.status === "open"){
               card.classList.add("open");
@@ -45,7 +56,7 @@ function displayIssues (issues) {
             <div class="p-3 flex flex-col gap-3 justify-center rounded-lg border-t-4 border-[#00A96E] w-full h-full bg-white shadow-md">
             <div class="flex justify-between items-center">
               <img src="./assets/Open-Status.png" alt="">
-              <div class="priority uppercase btn btn-soft">${issue.priority}</div>
+              <div class="priority uppercase btn btn-soft ${priorityClass}">${issue.priority}</div>
             </div>
             <p class="text-sm font-semibold">${issue.title}</p>
             <p class="text-xs text-[#64748B]">${issue.description}</p>
@@ -66,7 +77,7 @@ function displayIssues (issues) {
             <div class="p-3 flex flex-col gap-3 justify-center rounded-lg border-t-4 border-[#A855F7] w-full h-full bg-white shadow-md">
             <div class="flex justify-between items-center">
               <img src="./assets/Open-Status.png" alt="">
-              <div class="priority uppercase btn btn-soft">${issue.priority}</div>
+              <div class="priority uppercase btn btn-soft ${priorityClass}">${issue.priority}</div>
             </div>
             <p class="text-sm font-semibold">${issue.title}</p>
             <p class="text-xs text-[#64748B]">${issue.description}</p>
