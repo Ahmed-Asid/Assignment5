98aa25772f57f07b625cc2fcfb3788e8bc654546
Ahmed-Asid <ahmedasid249@gmail.com>
2026-03-10 06:33:52 +0600

    made the modal functional

--- main.js
+++ main.js
@@ -9,16 +9,10 @@ let currentTab = "allTab";
 active(currentTab);
 const issuesContainer = document.getElementById("cards-container");
 
-function displayIssues (issues) {
-    issuesContainer.innerHTML = "";
-
-    for (let issue of issues){
-        const card = document.createElement("div");
-      card.classList.add("card")
-
-        let labelHtml = "";
-
-        issue.labels.forEach(label => {
+const labelDesign = (obj) => {
+  let labelHtml = "";
+  let labelClass = "";
+        obj.labels.forEach(label => {
           console.log(label)
           if (label === "bug") {
             labelClass = "btn-error";
@@ -38,49 +32,52 @@ function displayIssues (issues) {
 
           labelHtml = labelHtml + `<div class="btn btn-soft text-xs uppercase ${labelClass}">${label}</div>`
         })
+        return labelHtml;
+}
 
-        
-        if (issue.priority === "high"){
+const priorityDesign = (obj) => {
+    let priorityHtml = "";
+    let priorityClass ="";
+  if (obj.priority === "high"){
           priorityClass = "btn-error"
         }
-        else if (issue.priority === "medium"){
+  else if (obj.priority === "medium"){
           priorityClass = "btn-warning"
         }
-        else if (issue.priority === "low"){
+  else if (obj.priority === "low"){
           priorityClass = "btn-accent"
         }
+      priorityHtml = `<div class="priority uppercase btn btn-soft ${priorityClass}">${obj.priority}</div>`;
+      return priorityHtml;
+}
+
+function displayIssues (issues) {
+    issuesContainer.innerHTML = "";
+    
+    for (let issue of issues){
+        const card = document.createElement("div");
+        card.classList.add("card")
+
+        const labelHtml = labelDesign(issue);
+
+        const priorityHtml = priorityDesign(issue); 
+
+        const borderColor = issue.status === "open" ? "border-[#00A96E]" : "border-[#A855F7]";
 
             if (issue.status === "open"){
               card.classList.add("open");
-            card.innerHTML = `
-            <div onclick="my_modal_5.showModal()" class="p-3 rounded-lg border-t-4 border-[#00A96E] w-full h-full bg-white shadow-md">
-            <div class="h-57 flex flex-col gap-3 justify-start">
-              <div class="flex justify-between items-center">
-              <img src="./assets/Open-Status.png" alt="">
-              <div class="priority uppercase btn btn-soft ${priorityClass}">${issue.priority}</div>
-              </div>
-              <p class="text-sm font-semibold">${issue.title}</p>
-              <p class="text-xs text-[#64748B] truncate">${issue.description}</p>
-              <div class="flex gap-2 flex-wrap">
-              ${labelHtml}
-              </div>
-            </div>
-            <div class="p-4 text-gray-500 text-xs border-t border-gray-300 space-y-2">
-              <p>#${issue.id} ${issue.author}</p>
-              <p>${issue.createdAt}</p>
-            </div>
-            </div>
-            `
             }
         
             else if (issue.status === "closed"){
               card.classList.add("closed")
+            }
+
             card.innerHTML = `
-            <div onclick="my_modal_5.showModal()" class="p-3 rounded-lg border-t-4 border-[#A855F7] w-full h-full bg-white shadow-md">
+            <div onclick="loadDetails(${issue.id})" class="p-3 rounded-lg border-t-4 ${borderColor} w-full h-full bg-white shadow-md">
             <div class="h-57 flex flex-col gap-3 justify-start">
               <div class="flex justify-between items-center">
               <img src="./assets/Open-Status.png" alt="">
-              <div class="priority uppercase btn btn-soft ${priorityClass}">${issue.priority}</div>
+              ${priorityHtml}
               </div>
               <p class="text-sm font-semibold">${issue.title}</p>
               <p class="text-xs text-[#64748B] truncate">${issue.description}</p>
@@ -92,12 +89,30 @@ function displayIssues (issues) {
               <p>#${issue.id} ${issue.author}</p>
               <p>${issue.createdAt}</p>
             </div>
-          </div>
+            </div>
             `
-            }
+
             issuesContainer.appendChild(card);
     }
 }
+
+const loadDetails = async (id) => {
+  const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+  const res = await fetch(url);
+  const details = await res.json();
+  displayDetails(details.data);
+}
+
+const displayDetails = (card) => {
+  console.log(card);
+    const modal = document.getElementById("details-container");
+    modal.innerHTML = "";
+    modal.innerHTML = `
+      <p class="font-bold text-2xl">${card.title}</p>
+    `;
+    document.getElementById("issue_modal").showModal();
+  }
+
 
 function active(tab) {
 
